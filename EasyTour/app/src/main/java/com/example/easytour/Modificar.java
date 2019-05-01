0ecfeecd7539cb94e6a3aecc51ab5d65f27d40f8
TuPeorPesadilla <miguelangelmurga1234@gmail.com>
2019-04-30 22:14:28 -0500

    Revert "Revert "Este commit es para poder avanzar en la lógica de la actividad editar perfil, lo que hice fue cambiar el nombre del las actividades ya que era un poco confuso, poder obtener los ides de los elementos que voy a usar y se hará para poder modificar la cuenta. (Voy a subir esta version ya que voy a salir).""
    This reverts commit 83ddbcce60e68662091f65beb9ca2d19897ab003.

--- EasyTour/app/src/main/java/com/example/easytour/Modificar.java
+++ EasyTour/app/src/main/java/com/example/easytour/Modificar.java
@@ -5,15 +5,23 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class turista extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    private TextView mTextMessage;
+public class Modificar extends AppCompatActivity{
+
+    private EditText et_email, et_confemail, et_pass, et_confpass;
+    private TextView tv_nombre, tv_apt, tv_apm, mTextMessage;
+    private Button b_edit;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        //Clase para poder hacer el movimiento entre pestañas
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
@@ -31,11 +39,20 @@ public class turista extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_turista);
+        setContentView(R.layout.activity_modificar);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_confemail = (EditText) findViewById(R.id.et_confemail);
+        et_pass = (EditText) findViewById(R.id.et_password);
+        et_confemail = (EditText) findViewById(R.id.et_confpass);
+        tv_nombre = (TextView) findViewById(R.id.et_nombres);
+        tv_apt = (TextView) findViewById(R.id.et_apt);
+        tv_apm = (TextView) findViewById(R.id.et_apm);
+
+        mTextMessage = (TextView) findViewById(R.id.tv_editarperfil);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
