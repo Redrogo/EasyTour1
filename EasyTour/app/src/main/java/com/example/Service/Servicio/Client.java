5e80f81b4ce03e1ec88b981c59a40091c459dffe
TuPeorPesadilla <miguelangelmurga1234@gmail.com>
2019-05-02 15:24:10 -0500

    Commit Mike11
    Aqui se empieza hacer el Servicio con ReactiveX java y con Retrofit2

---
+++ EasyTour/app/src/main/java/com/example/Service/Servicio/Client.java
@@ -0,0 +1,21 @@
+package com.example.Service.Servicio;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class Client {
+    private static Retrofit instance;
+
+    public static Retrofit getInstance(){
+        if(instance == null){
+            instance = new Retrofit.Builder()
+                    .baseUrl("http://192.168.0.10:3000/api/")
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return  instance;
+    }
+}
+
---
+++ EasyTour/app/src/main/java/com/example/Service/Servicio/Servicio.java
@@ -0,0 +1,16 @@
+package com.example.Service.Servicio;
+import java.util.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+import io.reactivex.Observable;
+
+public interface Servicio {
+
+    @GET("user")
+    Observable<String> loginUser(@Query("email") String email, @Query("password") String password);
+}
+
