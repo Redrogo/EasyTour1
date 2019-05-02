ecddcd7d56046a33c61d50464404a250501adc98
TuPeorPesadilla <miguelangelmurga1234@gmail.com>
2019-05-02 07:03:52 -0500

    Commit Mike (Commit 8)
    En este commit se arregló la aplicación Android, falta agregar la parte del Notificaciones

--- EasyTour/app/src/main/java/com/example/easytour/Notificaciones.java
+++ EasyTour/app/src/main/java/com/example/easytour/Notificaciones.java
@@ -1,52 +1,48 @@
 package com.example.easytour;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import org.w3c.dom.Text;
 
-public class Modificar extends AppCompatActivity{
+public class Notificaciones extends AppCompatActivity {
+
+    private TextView mTextMessage;
 
 
-    private EditText et_email, et_confemail, et_pass, et_confpass;
-    private TextView tv_nombre, tv_apt, tv_apm, tv_editar, tv_notificaciones;
-    private Button b_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_modificar);
+        setContentView(R.layout.activity_notificaciones);
+
+        mTextMessage = (TextView) findViewById(R.id.tv_noti);
+        mTextMessage.setText("Esta es la actividad de notificaciones");
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.it_editarperfil:
+                        Intent intent2 =  new Intent(Notificaciones.this, Modificar.class);
+                        startActivity(intent2);
                         break;
+
                     case R.id.it_notificaciones:
-                        Intent intent1 =  new Intent(Modificar.this, Notificaciones.class);
-                        startActivity(intent1);
                         break;
                 }
 
                 return false;
             }
         });
-
     }
 
 }
